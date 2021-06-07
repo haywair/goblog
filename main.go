611cf7a240e6fefabd92b29a3e14f21dd4fda463
lihongzhen <sinian1860@126.com>
2021-06-07 16:41:26 +0800

    404状态码

--- main.go
+++ main.go
@@ -6,12 +6,16 @@ import (
 )
 
 func hanleFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	if r.URL.Path == "/" {
-		fmt.Fprintln(w, "this ia welcome page celebrate")
+		fmt.Fprintln(w, "<h1>this ia welcome page celebrate</h1>")
 	} else if r.URL.Path == "/about" {
-		fmt.Fprintln(w, "yeah, this is a information page")
+		fmt.Fprintln(w, "<h2>yeah, this is a information page</h2>")
 	} else if r.URL.Path == "/company" {
 		fmt.Fprintln(w, "this is company information page")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintln(w, "the page is not exist")
 	}
 }
 
