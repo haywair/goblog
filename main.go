396e33ef075f118f6c8e340f9d93440ef9dde3b6
lihongzhen <sinian1860@126.com>
2021-06-17 13:55:41 +0800

    go web实战

--- main.go
+++ main.go
@@ -3,29 +3,65 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func hanleFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	if r.URL.Path == "/" {
-		fmt.Fprintln(w, "<h1>this ia welcome page celebrate</h1>")
-	} else if r.URL.Path == "/about" {
-		fmt.Fprintln(w, "<h2>yeah, this is a information page</h2>")
-	} else if r.URL.Path == "/company" {
-		fmt.Fprintln(w, "this is company information page")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintln(w, "the page is not exist")
-	}
+	fmt.Fprint(w, "<H1>欢迎来到blog</H1>")
 }
 
-func aboutHanlerFunc(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html;charset=utf-8")
-	fmt.Fprintln(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系")
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
+		"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
 }
 
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>请求页面未找到：(如有疑惑，请联系我们)</h1>")
+}
+
+func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	fmt.Fprint(w, "文章 ID:"+id)
+}
+
+func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "访问文章列表")
+}
+
+func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "创建新的文章")
+}
+
+func forceHTMLMiddleware(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+		h.ServeHTTP(w, r)
+	})
+}
 func main() {
-	http.HandleFunc("/", hanleFunc)
-	http.HandleFunc("/how", aboutHanlerFunc)
-	http.ListenAndServe(":3000", nil)
+	router := mux.NewRouter()
+	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
+	router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
+
+	router.HandleFunc("/articles/{id:[0-9]+}", articlesShowHandler).Methods("GET").Name("articles.show")
+	router.HandleFunc("/articles", articlesIndexHandler).Methods("GET").Name("articles.index")
+	router.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("articles.store")
+
+	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
+	router.Use(forceHTMLMiddleware)
+
+	homeURL, _ := router.Get("home").URL()
+	fmt.Println("homeUrl:", homeURL)
+
+	articlesURL, _ := router.Get("articles.show").URL("id", "23")
+	fmt.Println("articleURL:", articlesURL)
+
+	http.ListenAndServe(":3000", router)
 }
